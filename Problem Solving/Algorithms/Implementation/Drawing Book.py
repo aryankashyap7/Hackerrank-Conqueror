580c187e2630ea202262b22734e7d7bddb3f2c0d
Aryan Kashyap <75727351+aryankashyap7@users.noreply.github.com>
2021-06-11 21:25:03 +0530

    Updated Drawing Book.py

--- Problem Solving/Algorithms/Implementation/Drawing Book.py
+++ Problem Solving/Algorithms/Implementation/Drawing Book.py
@@ -1,8 +1,18 @@
-def pageCount(n, p):
-    a = (p-n)//2
-    b = n//2
-    return a if a<b else b
+#def pageCount(n, p):
+#    a = (p-n)//2
+#    b = n//2
+#    return a if a<b else b
 
-p = int(input())
-n = int(input())
-print(pageCount(n,p))
+#p = int(input())
+#n = int(input())
+#print(pageCount(n,p))
+
+
+def solve(n, p):
+    if(p<=n):
+        return min(p//2, n//2 - p//2)
+
+n = int(input().strip())
+p = int(input().strip())
+result = solve(n, p)
+print(result)
