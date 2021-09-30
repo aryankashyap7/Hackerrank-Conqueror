45a3f28968f221e4ba0d2f48961ce709acc40c84
Aryan Kashyap <akmail2017@gmail.com>
2021-09-30 23:32:40 +0530

    Added CamelCase

---
+++ Problem Solving/Algorithms/Strings/CamelCase.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string s;
+    int count = 0;
+    cin >> s;
+    for (int i = 0; i < s.size(); i++)
+    {
+        if (s[i] >= 65 && s[i] <= 90)
+            count++;
+    }
+
+    cout << count + 1 << "\n";
+    return 0;
+}
