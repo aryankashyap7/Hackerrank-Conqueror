e8b8b76c195c363fa0e90594a081122b58d33325
Aryan Kashyap <akmail2017@gmail.com>
2021-10-24 00:04:00 +0530

    Added String Split and Join

---
+++ Python/3. Strings/String Split and Join.py
@@ -0,0 +1,10 @@
+def split_and_join(line):
+    a=line
+    a = a.split()
+    a = "-".join(a)
+    return a
+
+if __name__ == '__main__':
+    line = input()
+    result = split_and_join(line)
+    print(result)
